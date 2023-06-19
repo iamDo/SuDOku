b10c8d3a7fc1ebe951fc1b405d7cef21fb08cc14
ppolmans <pieter.polmans@delta.blue>
2023-06-19 21:12:31 +0200

    generate a grid
    Signed-off-by: ppolmans <pieter.polmans@delta.blue>

--- gamegrid.lua
+++ gamegrid.lua
@@ -2,4 +2,39 @@ GameGrid = Square:extend()
 
 function GameGrid:new(x, y, length)
   GameGrid.super.new(self, x, y, length, 5)
+  self.subLineWidth = 3
+  self.cells = {}
+  self.cellLength = self.length/9
+
+  local cellX = self.x
+  local cellY = self.y
+  for i=1,9 do
+    self.cells[i] = {}
+    for j=1,9 do
+      local cell = Cell(cellX, cellY, self.cellLength, i)
+      self.cells[i][j] = cell
+      cellX = cellX + self.cellLength
+    end
+    cellX = self.x
+    cellY = cellY + self.cellLength
+  end
+end
+
+function GameGrid:draw()
+  GameGrid.super.draw(self)
+  for i=1,9 do
+    for j=1,9 do
+      self.cells[i][j]:draw()
+    end
+  end
+
+  love.graphics.setLineWidth(self.subLineWidth)
+  local subLineX = self.x + self.cellLength*3
+  love.graphics.line(subLineX, self.dims.top, subLineX, self.dims.bottom)
+  subLineX = subLineX + self.cellLength*3
+  love.graphics.line(subLineX, self.dims.top, subLineX, self.dims.bottom)
+  local subLineY = self.y + self.cellLength*3
+  love.graphics.line(self.dims.left, subLineY, self.dims.right, subLineY)
+  subLineY = subLineY + self.cellLength*3
+  love.graphics.line(self.dims.left, subLineY, self.dims.right, subLineY)
 end
