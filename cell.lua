d2ae055d50e8139975f80c2d8755050690b8b0fe
ppolmans <pieter.polmans@delta.blue>
2023-06-19 20:52:41 +0200

    draw number correctly
    Signed-off-by: ppolmans <pieter.polmans@delta.blue>

--- cell.lua
+++ cell.lua
@@ -3,10 +3,11 @@ Cell = Square:extend()
 function Cell:new(x, y, length, number)
   Cell.super.new(self, x, y, length, 1)
   self.number = number
-  self.textScale = self.length*0.05
+  self.textScale = 0.004*length -- magic number for scale chosen by visual testing
+  self.textOffset = -length
 end
 
 function Cell:draw()
   Cell.super.draw(self)
-  love.graphics.print(self.number, self.dims.left, self.dims.top, 0, self.textScale)
+  love.graphics.print(self.number, self.dims.left, self.dims.top, 0, self.textScale, self.textScale, self.textOffset)
  end
